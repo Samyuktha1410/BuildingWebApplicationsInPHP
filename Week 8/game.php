be0a9a4ee43ee3afed2430fef2874671d75cb8b1
Samyuktha1410 <50766407+Samyuktha1410@users.noreply.github.com>
2020-08-20 10:07:25 +0530

    Update game.php

--- Week 8/game.php
+++ Week 8/game.php
@@ -45,7 +45,7 @@ $result = check($computer, $human);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Anamitra Musib 32ce3337</title>
+<title>samyuktha</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
